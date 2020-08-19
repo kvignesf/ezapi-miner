8198610cc1d0523d123b641f4d8e862f80419245
shubham agarwal <mshubham1993@gmail.com>
2020-08-19 20:01:31 +0530

    Some utility functions added

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
-import json
-import jsonref
-
 from pprint import pprint
+
+import json
+import re
+
+import jsonref
 
 
 def deref_json(dict_data):
@@ -55,10 +57,79 @@ def extract_type_object(param_object, is_json_schema=False):
             result[key]["items"] = extract_type_array(val, is_json_schema)
 
         elif val["type"] == "object":
-            result[key]["properties"] = extract_type_object(val, is_json_schema)
+            result[key]["properties"] = extract_type_object(
+                val, is_json_schema)
 
         else:
             result[key]["format"] = val.get("format")
             result[key]["description"] = val.get("description")
 
     return result
+
+
+def camel_case_split(word):
+    try:
+        word = word[0].upper() + word[1:]
+        res = re.findall(r'[A-Z](?:[a-z]+|[A-Z]*(?=[A-Z]|$))', word)
+        return (" ".join(res)).lower()
+    except:
+        return None
+
+
+# Reference - https://github.com/TimKam/compound-word-splitter
+# enchant installed required - https://abiword.github.io/enchant/
+def word_split(word):
+    try:
+        import splitter
+        word = word.lower()
+        res = splitter.split(word)
+        if "".join(res) == word:
+            return " ".join(res)
+        return None
+    except:
+        return None
+
+
+def split_operation_id(operationId):
+    res1 = camel_case_split(operationId)
+    res2 = word_split(operationId)
+
+    if res1 and res2:
+        if res1 == res2:
+            return res1
+        elif res1.lower() == operationId.lower():
+            return res2
+        else:
+            return res1
+    elif res1:
+        return res1
+    elif res2:
+        return res2
+    else:
+        return operationId
+
+
+def extract_apiops_description(operationId, description, summary):
+    if operationId:
+        return split_operation_id(operationId)
+    elif description or summary:
+        if not description:
+            return summary
+        elif not summary:
+            return description
+        else:
+            if len(description) < len(summary):
+                return description
+            else:
+                return summary
+    return None
+
+
+# JFT - Petstore
+"""
+T = ["uploadFile", "addPet", "updatePet", "findPetsByStatus", "findPetsByTags", "getPetById", "updatePetWithForm", "deletePet", "placeOrder", "getOrderById", "deleteOrder",
+     "getInventory", "createUsersWithArrayInput", "createUsersWithListInput", "getUserByName", "updateUser", "deleteUser", "loginUser", "logoutUser", "createUser"]
+
+for t in T:
+    print(t, split_operation_id(t))
+"""
