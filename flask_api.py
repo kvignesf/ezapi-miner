b655219b33504c9ccec3621ce7aab7767c55762a
shubham agarwal <mshubham1993@gmail.com>
2020-08-19 20:04:21 +0530

    Flask API added

---
+++ flask_api.py
@@ -0,0 +1,29 @@
+from swagger_parser import parse_swagger_api
+from param_functions import handle_param_functions
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/swagger_parser", methods=["POST"])
+def swagger_parser():
+    f = request.files["file"]
+
+    filepath = "./files/inputfile.json"
+    f.save(filepath)
+
+    res = parse_swagger_api(filepath)
+    return jsonify(res)
+
+
+@app.route("/param_functions", methods=["POST"])
+def param_functions():
+    request_data = request.get_json()
+    api_ops_id = request_data.get("api_ops_id")
+    res = handle_param_functions(api_ops_id)
+    return jsonify(res)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
