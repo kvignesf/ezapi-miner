9039c0cd4583f9dba7887c67d908057bdf75cd85
shubham agarwal <mshubham1993@gmail.com>
2020-08-21 19:06:15 +0530

    Flask API for Sankey Visualization Added

--- flask_api.py
+++ flask_api.py
@@ -1,9 +1,12 @@
 from swagger_parser import parse_swagger_api
 from param_functions import handle_param_functions
+from visualizer import fetch_sankey_data
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route("/")
@@ -23,11 +26,26 @@ def swagger_parser():
 
 
 @app.route("/param_functions", methods=["POST"])
+@cross_origin(supports_credentials=True)
 def param_functions():
     request_data = request.get_json()
     api_ops_id = request_data.get("api_ops_id")
     res = handle_param_functions(api_ops_id)
     return jsonify(res)
+
+
+@app.route("/visualizer", methods=["GET"])
+def visualizer_function():
+    request_data = dict(request.args)
+    if 'api_ops_id' in request_data:
+        api_ops_id = str(request_data['api_ops_id'])
+    if 'tag' in request_data:
+        tag = str(request_data['tag'])
+
+    print(api_ops_id, tag)
+
+    result = fetch_sankey_data(api_ops_id, tag)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
