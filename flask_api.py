18f50542c1105db7f9408ec33e9296d3fc785681
shubham agarwal <mshubham1993@gmail.com>
2020-09-02 08:32:50 +0530

    Parser and Extraction APIs are combined

--- flask_api.py
+++ flask_api.py
@@ -1,6 +1,7 @@
-from swagger_parser import parse_swagger_api
-from param_functions import handle_param_functions
-from visualizer import fetch_sankey_data
+# from swagger_parser import parse_swagger_api
+# from param_functions import handle_param_functions
+# from visualizer import fetch_sankey_data
+from apifunctions import parse_swagger_openapi, get_sankey_data
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
@@ -14,38 +15,59 @@ def home():
     return "Hello APIOPS"
 
 
-@app.route("/swagger_parser", methods=["POST"])
-def swagger_parser():
-    print("In swagger parser")
-    print(request.files)
+@app.route("/apiops_parser", methods=["POST"])
+def apiops_parser():
     f = request.files["file"]
 
     filepath = "./files/inputfile.json"
     f.save(filepath)
 
-    res = parse_swagger_api(filepath)
-    return jsonify(res)
-
-
-@app.route("/param_functions", methods=["POST"])
-@cross_origin(supports_credentials=True)
-def param_functions():
-    request_data = request.get_json()
-    api_ops_id = request_data.get("api_ops_id")
-    res = handle_param_functions(api_ops_id)
+    res = parse_swagger_openapi(filepath)
     return jsonify(res)
 
 
 @app.route("/visualizer", methods=["GET"])
-def visualizer_function():
+def visualizer():
     api_ops_id = str(request.args.get('api_ops_id'))
-    tags = request.args.getlist('tag', None)
 
     if not api_ops_id:
-        return jsonify({"success": false, "status": 500, "message": "api ops id is missing"})
+        return jsonify({"success": false, "status": 500, "message": "apiops id is missing"})
 
-    result = fetch_sankey_data(api_ops_id, tags)
+    result = get_sankey_data(api_ops_id)
     return jsonify(result)
+
+
+# @app.route("/swagger_parser", methods=["POST"])
+# def swagger_parser():
+#     print("In swagger parser")
+#     print(request.files)
+#     f = request.files["file"]
+
+#     filepath = "./files/inputfile.json"
+#     f.save(filepath)
+
+#     res = parse_swagger_api(filepath)
+#     return jsonify(res)
+
+
+# @app.route("/param_functions", methods=["POST"])
+# def param_functions():
+#     request_data = request.get_json()
+#     api_ops_id = request_data.get("api_ops_id")
+#     res = handle_param_functions(api_ops_id)
+#     return jsonify(res)
+
+
+# @app.route("/visualizer", methods=["GET"])
+# def visualizer_function():
+#     api_ops_id = str(request.args.get('api_ops_id'))
+#     tags = request.args.getlist('tags', None)
+
+#     if not api_ops_id:
+#         return jsonify({"success": false, "status": 500, "message": "api ops id is missing"})
+
+#     result = fetch_sankey_data(api_ops_id, tags)
+#     return jsonify(result)
 
 
 if __name__ == "__main__":
