1a886c238eac9191eda097d66db91ae7db255f16
shubham agarwal <mshubham1993@gmail.com>
2021-07-15 19:40:29 +0530

    Sankey generation fixed

--- api_designer/visualizer/element_scoring.py
+++ api_designer/visualizer/element_scoring.py
@@ -35,6 +35,34 @@ def deref_table_body_data(body):
 def enhance_attributes(projectid, db, project_type, filename=None):
     try:
         paths = db.operationdatas.find({"projectid": projectid})
+        project_data = db.projects.find_one({"projectId": projectid})
+
+        project_resources = project_data.get("resources", [])
+        project_resources = [x["resource"] for x in project_resources]
+
+        resource_data = db.resources.find({"resourceId": {"$in": project_resources}})
+        resource_data = list(resource_data)
+
+        resource_dict = {}
+        for r in resource_data:
+            resource_path = r["path"]
+            for rp in resource_path:
+                resource_name = r["resourceName"]
+                rp_endpoint = rp["pathName"]
+
+                rp_operations = rp.get("operations", [])
+                for rpo in rp_operations:
+                    op_method = rpo["operationType"]
+                    op_name = rpo["operationName"]
+                    op_desc = rpo["operationDescription"]
+
+                    resource_dict_key = op_method + "__" + rp_endpoint
+                    resource_dict[resource_dict_key] = {
+                        "tag": resource_name,
+                        "summary": op_name,
+                        "description": op_desc,
+                    }
+
         paths = list(paths)
         if not paths:
             return False, "project data not found"
@@ -53,8 +81,28 @@ def enhance_attributes(projectid, db, project_type, filename=None):
 
             endpoint = path_data["endpoint"]
             method = path_data["method"]
-            summary = path_data.get("summary")
-            description = path_data.get("description")
+
+            tmp_endpoint = endpoint.strip("/")
+            tmp_endpoint = tmp_endpoint.split("/")[0]
+
+            lookup_key = method.upper() + "__" + tmp_endpoint
+
+            try:
+                tag = resource_dict.get(lookup_key).get("tag")
+            except:
+                tag = "API Model"
+
+            try:
+                summary = resource_dict.get(lookup_key).get("summary")
+            except:
+                summary = method.upper() + " " + endpoint
+
+            try:
+                description = resource_dict.get(lookup_key).get("description")
+            except:
+                description = method.upper() + " " + endpoint
+
+            tags = [tag]
 
             path_request_data = path_data["requestData"]
             path_param_data = (
@@ -70,10 +118,9 @@ def enhance_attributes(projectid, db, project_type, filename=None):
             else:
                 if "body" in path_request_data and path_request_data["body"]:
                     body_data = deref_body_data(path_request_data["body"], all_schemas)
-                    print(body_data)
                     path_param_data += [body_data]
 
-            tags = path_data.get("tags", ["API Model"])
+            # tags = path_data.get("tags", ["API Model"])
             for t in tags:
                 if t not in tag_wise_attributes:
                     tag_wise_attributes[t] = {}
@@ -86,7 +133,6 @@ def enhance_attributes(projectid, db, project_type, filename=None):
                         param = param["items"]
 
                     if "type" not in param:  # Only flat keys
-
                         for k, v in param.items():
                             if k not in tag_wise_attributes[t]:
                                 tag_wise_attributes[t][k] = []
@@ -99,6 +145,19 @@ def enhance_attributes(projectid, db, project_type, filename=None):
                                     "description": description,
                                 }
                             )
+                    else:
+                        k = "other"
+                        if k not in tag_wise_attributes[t]:
+                            tag_wise_attributes[t][k] = []
+
+                        tag_wise_attributes[t][k].append(
+                            {
+                                "endpoint": endpoint,
+                                "method": method,
+                                "summary": summary,
+                                "description": description,
+                            }
+                        )
 
         tag_wise_attributes = transform_ds(tag_wise_attributes)
 
@@ -108,7 +167,6 @@ def enhance_attributes(projectid, db, project_type, filename=None):
             "api_ops_id": projectid,
             "data": tag_wise_attributes,
         }
-        pprint(tag_wise_attributes)
         config.store_document(element_collction, element_document, db)
 
         return True, "ok"
